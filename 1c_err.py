d6b6700a88461970f8d3bdaf4dcfd01f31984609
pietro221 <129070960+pietro221@users.noreply.github.com>
2025-03-25 11:02:14 +0100

    Update 1c_err.py

--- 1c_err.py
+++ 1c_err.py
@@ -12,17 +12,18 @@ def get_memory_usage():
     memory_info = process.memory_info()
     return memory_info.rss / (1024 ** 2)
 
+#Constants
+#We use natural units
 m = 1
 w = 1
 hcut = 1
 
-N_Alpha = 40
-N_Cycles = 100000
-timestep = 0.01
-Nvalues = np.array([1, 10, 100, 500])
-Therm_steps = 20000
-sigma_gaussian = 1
-block_size = 10000  # Dimensione del blocco
+N_Alpha = 40 # Number of Alpha points
+N_Cycles = 100000 # Number of Monte Carlo Cycles
+Time_Step = 0.01 # Same timestep for each N° of harmonic oscillator
+N_Values = np.array([1, 10, 100, 500]) # N° of harmonic oscillator
+Therm_Steps = 20000 #Thermalization steps
+block_size = 10000  # Size of Tau blocks for error analysis
 
 @njit
 def WaveFunction(x, alpha):
@@ -34,7 +35,7 @@ def LocalEnergy(x, alpha, D):
 
 def RandomMatrix(N_Cycles, N, D):
     rng = np.random.default_rng(41)
-    return np.random.normal(0, sigma_gaussian, (N_Cycles, N, D))
+    return np.random.normal(0, 1, (N_Cycles, N, D))
 
 @njit
 def DriftForce(x: np.ndarray, alpha: float):
@@ -42,15 +43,16 @@ def DriftForce(x: np.ndarray, alpha: float):
 
 @njit
 def GF(xOld: np.ndarray, xNew: np.ndarray, F: np.ndarray, alpha):
-    return np.exp(-(0.5 * (0.25 * timestep * (np.sum(DriftForce(xOld, alpha)**2, axis=1) - np.sum(DriftForce(xNew, alpha)**2, axis=1)) + np.sum((xOld - xNew) * (DriftForce(xNew, alpha) - DriftForce(xOld, alpha)), axis=1))))
+    return np.exp(-(0.5 * (0.25 * Time_Step * (np.sum(DriftForce(xOld, alpha)**2, axis=1) - np.sum(DriftForce(xNew, alpha)**2, axis=1))
+                           + np.sum((xOld - xNew) * (DriftForce(xNew, alpha) - DriftForce(xOld, alpha)), axis=1))))
 
 @njit
 def MonteCarloSampling(Alpha, Alpha_Pos, N_Pos, MCMatrix, xOld, MCEnergy, total_steps, rejected_steps, D):
     psiOld = WaveFunction(xOld, Alpha)
 
     for j in range(1, N_Cycles):
-        if j < Therm_steps:
-            xNew = xOld + 0.5 * DriftForce(xOld, Alpha) * timestep + MCMatrix[j, :, :] * sqrt(timestep)
+        if j < Therm_Steps:
+            xNew = xOld + 0.5 * DriftForce(xOld, Alpha) * Time_Step + MCMatrix[j, :, :] * sqrt(Time_Step)
             psiNew = WaveFunction(xNew, Alpha)
             
             if np.any(psiOld<0):
@@ -58,39 +60,40 @@ def MonteCarloSampling(Alpha, Alpha_Pos, N_Pos, MCMatrix, xOld, MCEnergy, total_
             if np.any(psiOld==0):
                 return np.ones(N_Alpha+1),0,0,MCEnergy
             
-            moves = (((psiNew**2) * GF(xOld, xNew, DriftForce(xOld, Alpha), Alpha)) / ((psiOld**2))) - np.random.normal(0, sigma_gaussian)
+            moves = (((psiNew**2) * GF(xOld, xNew, DriftForce(xOld, Alpha), Alpha)) / ((psiOld**2))) - np.random.normal(0, 1)
             moves = np.where(moves > 0, 1, 0)
             matrixmoves = np.ones((xOld.shape[0], D)) * moves[:, np.newaxis]
             xOld[:, :] += (xNew - xOld) * matrixmoves
             psiOld = psiNew
         else:
-            xNew = xOld + 0.5 * DriftForce(xOld, Alpha) * timestep + MCMatrix[j, :, :] * sqrt(timestep)
+            xNew = xOld + 0.5 * DriftForce(xOld, Alpha) * Time_Step + MCMatrix[j, :, :] * sqrt(Time_Step)
             psiNew = WaveFunction(xNew, Alpha)
             total_steps += xOld.shape[0]
-            
+
+            #debugging
             if np.any(psiOld<0):
                 return np.zeros(N_Alpha+1),0,0,MCEnergy
             if np.any(psiOld==0):
                 return np.ones(N_Alpha+1),0,0,MCEnergy
             
-            moves = (((psiNew**2) * GF(xOld, xNew, DriftForce(xOld, Alpha), Alpha)) / ((psiOld**2))) - np.random.normal(0, sigma_gaussian)
+            moves = (((psiNew**2) * GF(xOld, xNew, DriftForce(xOld, Alpha), Alpha)) / ((psiOld**2))) - np.random.normal(0, 1)
             moves = np.where(moves > 0, 1, 0)
             matrixmoves = np.ones((xOld.shape[0], D)) * moves[:, np.newaxis]
             rejected_steps += np.sum(1 - moves)
             if np.all(moves != 1):
-                MCEnergy[Alpha_Pos, j - Therm_steps] = MCEnergy[Alpha_Pos, j - Therm_steps - 1]
+                MCEnergy[Alpha_Pos, j - Therm_Steps] = MCEnergy[Alpha_Pos, j - Therm_Steps - 1]
             else:
                 xOld += (xNew - xOld) * matrixmoves
                 psiOld = WaveFunction(xOld, Alpha)
-                MCEnergy[Alpha_Pos, j - Therm_steps] = LocalEnergy(xOld, Alpha, D)
+                MCEnergy[Alpha_Pos, j - Therm_Steps] = LocalEnergy(xOld, Alpha, D)
 
-    MeanEnergy = np.sum(MCEnergy, axis=1) / (N_Cycles - Therm_steps)
+    MeanEnergy = np.sum(MCEnergy, axis=1) / (N_Cycles - Therm_Steps)
     return MeanEnergy, total_steps, rejected_steps, MCEnergy
 
 @njit(parallel=True)
 def ErrorHandling(MCEnergy_Alpha):
     MeanEnergy = np.sum(MCEnergy_Alpha) / MCEnergy_Alpha.shape[0]
-    sigma2 = np.sum((MCEnergy_Alpha - MeanEnergy) ** 2) / MCEnergy_Alpha.shape[0]
+    #sigma2 = np.sum((MCEnergy_Alpha - MeanEnergy) ** 2) / MCEnergy_Alpha.shape[0]
 
     numerator = 0.0
     n = MCEnergy_Alpha.shape[0]
@@ -109,29 +112,29 @@ def ErrorHandling(MCEnergy_Alpha):
             sum_corr /= (n - tau)
             numerator += sum_corr - MeanEnergy**2
 
-    numerator /= sigma2
-
-    return sigma2, numerator
+    return numerator
 
 start_time = time.time()
 for D in [1, 2, 3]:
     print(f"Computing Energies in {D} dimensions...")
-    for N_Pos, N in enumerate(Nvalues):
+    for N_Pos, N in enumerate(N_Values):
         total_steps = 0
         rejected_steps = 0
         alpha_values = np.linspace(0.7, 1.3, N_Alpha)
-        MCEnergy = np.zeros((N_Alpha, N_Cycles - Therm_steps))
+        MCEnergy = np.zeros((N_Alpha, N_Cycles - Therm_Steps))
         sigma2 = np.zeros(N_Alpha)
         tau_bar = np.zeros(N_Alpha)
 
         for Alpha_Pos in tqdm(range(N_Alpha)):
             Alpha = alpha_values[Alpha_Pos]
-            xOld = timestep * (np.random.normal(0, sigma_gaussian, (N, D)))
+            xOld = Time_Step * (np.random.normal(0, 1, (N, D)))
             psiOld = WaveFunction(xOld, Alpha)
             MCEnergy[Alpha_Pos, 0] = LocalEnergy(xOld, Alpha, D)
             MCMatrix = RandomMatrix(N_Cycles, N, D)
             MeanEnergy, total_steps, rejected_steps, MatrixEnergy = MonteCarloSampling(Alpha, Alpha_Pos, N_Pos, MCMatrix, xOld, MCEnergy, total_steps, rejected_steps, D)
             MCEnergy = MatrixEnergy
+
+            #Debugging
             if MeanEnergy.shape[0]==N_Alpha+1:
                 if MeanEnergy==np.zeros(N_Alpha+1):
                     print("The wavefunction is negative")
@@ -139,11 +142,11 @@ for D in [1, 2, 3]:
                 if MeanEnergy==np.ones(N_Alpha+1):
                     print("Division by 0: the Wavefunction is too small")
                     sys.exit()
-            if not ((N_Cycles-Therm_steps)/block_size).is_integer():
+            if not ((N_Cycles-Therm_Steps)/block_size).is_integer():
                 print(f"Number of MC Cycles is not a multiple of {block_size}: the number of blocks is not an integer")
                 sys.exit()
             if D==3:
-                sigma2[Alpha_Pos], tau_bar[Alpha_Pos] = ErrorHandling(MCEnergy[Alpha_Pos, :])
+                 tau_bar[Alpha_Pos] = ErrorHandling(MCEnergy[Alpha_Pos, :])
 
         rejection_percentage = (rejected_steps / total_steps) * 100
         print(f"Rejection Percentage for N={N}: {rejection_percentage:.2f}%")
@@ -151,7 +154,7 @@ for D in [1, 2, 3]:
         min_index = np.argmin(MeanEnergy)
         best_alpha = alpha_values[min_index]
         min_energy = MeanEnergy[min_index]       
-        err = np.sqrt(sigma2 * np.abs(tau_bar) / (N_Cycles - Therm_steps))
+        err = np.sqrt(np.abs(tau_bar) / (N_Cycles - Therm_Steps))
         min_error = err[min_index]
         print(f"Optimal alpha for N={N}: {best_alpha}, Minimum energy: {min_energy}")
         if D==3:

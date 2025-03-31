43360748e8123acd69fd22308c1d1b30ca781713
Gabriele Novelli <130922361+GabrieleNovelli@users.noreply.github.com>
2025-03-31 15:40:44 +0200

    Update 1b_errs.py
    FINE VERA

--- codes/1b_errs.py
+++ codes/1b_errs.py
@@ -98,8 +98,7 @@ for D in [1,2,3]: # Cycle through dimensions
     
     # Debugging: checking that blocks in error handling are well defined
     if not ((N_Cycles-Therm_Steps)/block_size).is_integer():
-        print(f"Number of MC Cycles is not a multiple of {block_size}: the number of blocks is not an integer")
-        sys.exit()
+        raise ValueError(f"Number of MC Cycles is not a multiple of {block_size}: the number of blocks is not an integer")
     
     print(f"Computing Energies in {D} dimensions...")
 
